2c10493254e3861d3c8054ef5b605c1adfa2c701
FathurRahman0 <rahmanfthr165@gmail.com>
2025-05-07 15:29:20 +0700

    menemukan elemen pada array

--- pertemuan7.cpp
+++ pertemuan7.cpp
@@ -76,5 +76,28 @@ void binarySearch()
         cout << "Masukkan elemen yang ingin dicari : ";
         cin >> x;
 
-    } 
-}
+        int low = 0;
+        int high = nPanjang - 1;
+
+        do
+        {
+            int mid = (low + high) / 2;
+            if (element[mid] == x)
+            {
+                cout << "\n[✓] Elemen " << x << " ditemukan pada indeks " << mid << ".\n";
+                return;
+            }
+            if (x < element[mid])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        } while (low <= high);
+
+        
+    }
+} 
+
